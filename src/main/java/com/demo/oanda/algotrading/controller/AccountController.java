bbe2e3bf7e05fbc769ef1d624f0915103196a629
Sean Mayer <apart-things.0p@icloud.com>
2024-09-25 17:08:57 +0100

    Refactor AccountController to add OpenAPI documentation and operation summaries

--- src/main/java/com/demo/oanda/algotrading/controller/AccountController.java
+++ src/main/java/com/demo/oanda/algotrading/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.oanda.v20.account.Account;
 import com.oanda.v20.account.AccountProperties;
 import com.oanda.v20.account.AccountSummary;
 import com.oanda.v20.primitives.Instrument;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,21 +23,25 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Get Account Details", description = "Get details of a specific account")
     @GetMapping("/account/{accountId}")
     public Account getAccountDetails(@PathVariable String accountId) throws ExecuteException, RequestException {
         return accountService.getAccountDetails(accountId);
     }
 
+    @Operation(summary = "Get Accounts", description = "List all accounts")
     @GetMapping("/accounts")
     public List<AccountProperties> getAccounts() throws ExecuteException, RequestException {
         return accountService.getAccounts();
     }
 
+    @Operation(summary = "Get Account Instruments", description = "List all instruments available for a specific account")
     @GetMapping("/account/{accountId}/instruments")
     public List<Instrument> getAccountInstruments(@PathVariable String accountId) throws ExecuteException, RequestException {
         return accountService.getAccountInstruments(accountId);
     }
 
+    @Operation(summary = "Get Account Summary", description = "Get a summary of a specific account")
     @GetMapping("/account/{accountId}/summary")
     public AccountSummary getAccountSummary(@PathVariable String accountId) throws ExecuteException, RequestException {
         return accountService.getAccountSummary(accountId);

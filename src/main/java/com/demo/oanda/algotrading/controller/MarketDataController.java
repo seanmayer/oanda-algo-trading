ca3f76c3a7a67f6b2b391138ad0c7030136ff5b1
Sean Mayer <apart-things.0p@icloud.com>
2024-09-26 17:29:02 +0100

    Add OpenAPI documentation to MarketDataController

--- src/main/java/com/demo/oanda/algotrading/controller/MarketDataController.java
+++ src/main/java/com/demo/oanda/algotrading/controller/MarketDataController.java
@@ -4,6 +4,7 @@ import com.demo.oanda.algotrading.service.MarketDataService;
 import com.oanda.v20.ExecuteException;
 import com.oanda.v20.RequestException;
 import com.oanda.v20.instrument.Candlestick;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class MarketDataController {
         this.marketDataService = marketDataService;
     }
 
+    @Operation(summary = "Get Market Data", description = "Get market data for the specified instrument")
     @GetMapping("/marketdata/{instrument}")
     public List<Candlestick> getMarketData(@PathVariable String instrument) throws ExecuteException, RequestException {
         return marketDataService.getMarketData(instrument);

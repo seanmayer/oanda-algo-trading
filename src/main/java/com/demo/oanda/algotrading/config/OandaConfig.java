a9fb0e779ab1af452cdc8d8ae40cb17dc6a576c8
Sean Mayer <apart-things.0p@icloud.com>
2024-09-30 17:19:28 +0100

    Tidy up

--- src/main/java/com/demo/oanda/algotrading/config/OandaConfig.java
+++ src/main/java/com/demo/oanda/algotrading/config/OandaConfig.java
@@ -13,12 +13,9 @@ public class OandaConfig {
     private String apiKey;
 
     public OandaConfig() {
-        // Directly fetch the environment variable
         this.apiKey = System.getenv("OANDA_API_KEY");
         if (this.apiKey == null) {
             logger.severe("OANDA_API_KEY environment variable is not set.");
-        } else {
-            logger.info("Using API token: Bearer " + this.apiKey);
         }
     }
 
@@ -32,6 +29,4 @@ public class OandaConfig {
     public String getApiKey() {
         return apiKey;
     }
-
-    // No setter needed if you're directly setting apiKey via the constructor or in a method
 }

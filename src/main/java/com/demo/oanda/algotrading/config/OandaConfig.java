4a12d2b0f059c6920e7d416fe2d1082348b98798
Sean Mayer <apart-things.0p@icloud.com>
2024-10-01 20:05:38 +0100

    Improved config feedback

--- src/main/java/com/demo/oanda/algotrading/config/OandaConfig.java
+++ src/main/java/com/demo/oanda/algotrading/config/OandaConfig.java
@@ -11,19 +11,28 @@ public class OandaConfig {
 
     private static final Logger logger = Logger.getLogger(OandaConfig.class.getName());
     private String apiKey;
+    private String url;
 
     public OandaConfig() {
         this.apiKey = System.getenv("OANDA_API_KEY");
+        this.url = "https://api-fxpractice.oanda.com";
         if (this.apiKey == null) {
             logger.severe("OANDA_API_KEY environment variable is not set.");
+        }
+        if (this.url == null) {
+            logger.severe("OANDA_API_URL environment variable is not set.");
         }
     }
 
     @Bean
     public Context context() {
-        String url = "https://api-fxpractice.oanda.com";
         String token = getApiKey();
+        String url = getUrl();
         return new Context(url, token);
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public String getApiKey() {

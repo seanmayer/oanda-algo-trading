484c0b514dbefb8e0d1157435b4428d4b66728b0
Sean Mayer <apart-things.0p@icloud.com>
2024-10-05 22:47:29 +0100

    Provided immutability final variables to prevent bugs and increase thread safety

--- src/main/java/com/demo/oanda/algotrading/service/AccountService.java
+++ src/main/java/com/demo/oanda/algotrading/service/AccountService.java
@@ -16,49 +16,49 @@ public class AccountService {
     private static final Logger logger = Logger.getLogger(AccountService.class.getName());
     private final Context context;
 
-    public AccountService(Context context) {
+    public AccountService(final Context context) {
         this.context = context;
     }
 
     public List<AccountProperties> getAccounts() throws ExecuteException, RequestException {
         try {
-            AccountListResponse response = context.account.list();
+            final AccountListResponse response = context.account.list();
             logger.info("Received accounts response from Oanda API: " + response);
             return response.getAccounts();
-        } catch (RequestException e) {
+        } catch (final RequestException e) {
             logger.severe("RequestException occurred while fetching accounts: " + e.getMessage());
             throw e;
         }
     }
 
-    public Account getAccountDetails(String accountId) throws ExecuteException, RequestException {
+    public Account getAccountDetails(final String accountId) throws ExecuteException, RequestException {
         try {
-            AccountGetResponse accountResponse = context.account.get(new AccountID(accountId));
+            final AccountGetResponse accountResponse = context.account.get(new AccountID(accountId));
             logger.info("Received account response from Oanda API: " + accountResponse);
             return accountResponse.getAccount();
-        } catch (RequestException e) {
+        } catch (final RequestException e) {
             logger.severe("RequestException occurred while fetching account details: " + e.getMessage());
             throw e;
         }
     }
 
-    public AccountSummary getAccountSummary(String accountId) throws ExecuteException, RequestException {
+    public AccountSummary getAccountSummary(final String accountId) throws ExecuteException, RequestException {
         try {
-            AccountSummaryResponse response = context.account.summary(new AccountID(accountId));
+            final AccountSummaryResponse response = context.account.summary(new AccountID(accountId));
             logger.info("Received account summary from Oanda API: " + response);
             return response.getAccount();
-        } catch (RequestException e) {
+        } catch (final RequestException e) {
             logger.severe("RequestException occurred while fetching account summary: " + e.getMessage());
             throw e;
         }
     }
 
-    public List<Instrument> getAccountInstruments(String accountId) throws ExecuteException, RequestException {
+    public List<Instrument> getAccountInstruments(final String accountId) throws ExecuteException, RequestException {
         try {
-            AccountInstrumentsResponse response = context.account.instruments(new AccountID(accountId));
+            final AccountInstrumentsResponse response = context.account.instruments(new AccountID(accountId));
             logger.info("Received instruments response from Oanda API: " + response);
             return response.getInstruments();
-        } catch (RequestException e) {
+        } catch (final RequestException e) {
             logger.severe("RequestException occurred while fetching instruments: " + e.getMessage());
             throw e;
         }
